05d0f50092ecc3b65055a232cc98707cd374e245
davidwis <davidwis000@gmail.com>
2024-03-10 16:22:37 -0300

    Ej 2a

--- main.py
+++ main.py
@@ -15,17 +15,29 @@ with open("pokemon.json", "r") as c:
 with open("pokeballs.json", "r") as c:
     pokeballs = json.load(c)
 
-def createMatrix(tries):
+def create_pokemon_pokeball_matrix(tries, level = 100, status_effect = StatusEffect.NONE, health_points = 1):
     pokemon_pokeball_matrix = {} 
 
     for name in pokemon_names:
         pokemon_pokeball_matrix[name] = {}
-        pokemon = factory.create(name, 100, StatusEffect.NONE, 1)
+        pokemon = factory.create(name, level, status_effect, health_points)
         for ball in pokeballs:
             pokemon_pokeball_matrix[name][ball] = 0
             for _ in range(tries):
                 pokemon_pokeball_matrix[name][ball] += attempt_catch(pokemon, ball)[0]
     return pokemon_pokeball_matrix
+
+def create_pokemon_status_matrix(tries, level = 100, ball = 'pokeball', health_points = 1):
+    pokemon_status_matrix = {} 
+
+    for name in pokemon_names:
+        pokemon_status_matrix[name] = {}
+        for status_effect in StatusEffect:
+            pokemon = factory.create(name, level, status_effect, health_points)
+            pokemon_status_matrix[name][status_effect] = 0
+            for _ in range(tries):
+                pokemon_status_matrix[name][status_effect] += attempt_catch(pokemon, ball)[0]
+    return pokemon_status_matrix
 
 
 # In[1]: Exercise 1a
@@ -35,7 +47,7 @@ TRIES = 100
 
 
 pokeball_stats = {}
-pokemon_pokeball_matrix = createMatrix(TRIES)
+pokemon_pokeball_matrix = create_pokemon_pokeball_matrix(tries=TRIES)
 df = pd.DataFrame(pokemon_pokeball_matrix).transpose()
 
 for ball in pokeballs:
@@ -49,7 +61,7 @@ catchRateDf
 
 TRIES = 1000
 
-pokemon_pokeball_matrix = createMatrix(TRIES)
+pokemon_pokeball_matrix = create_pokemon_pokeball_matrix(tries=TRIES)
 
 
 df = pd.DataFrame(pokemon_pokeball_matrix)
@@ -79,4 +91,30 @@ snorlax_stats_df.plot.bar(x="Pokeball", y="Efficiency",
                           legend=False)
 plt.show()
 
-# %%
+#In[3] Exercise 2a
+
+TRIES = 10_000
+
+pokemon_status_matrix = create_pokemon_status_matrix(tries=TRIES)
+
+df = pd.DataFrame(pokemon_status_matrix)
+odf = df.copy()
+
+df = df.apply(lambda x: x / x[-1])
+df
+
+status_effect_stats = df.mean(axis=1).to_frame()
+status_effect_stats.columns = ["Average"]
+status_effect_stats["Standard Deviation"] = df.std(axis=1)
+status_effect_stats = status_effect_stats[:-1]
+status_effect_stats.plot.bar(y="Average", 
+                            yerr="Standard Deviation",
+                            title="Average and standard deviation of status effects",
+                            color=['blue', 'red', 'green', 'orange', 'purple'],
+                            legend=False)
+
+
+
+
+# In[4] Exercise 2b 
+
