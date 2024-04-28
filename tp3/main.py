e4f637f46e9d41d639d6dfc35c9c63c7c1678123
davidwis <davidwis000@gmail.com>
2024-04-28 15:58:39 -0300

    Algo de modularizacion

--- tp3/main.py
+++ tp3/main.py
@@ -30,6 +30,13 @@ def ejercicio_1():
     print(f"x2 = {-w_and[1]/w_and[2]}*x1 + {-w_and[0]/w_and[2]}")
     print(f"x2 = {-w_or[1]/w_or[2]}*x1 + {-w_or[0]/w_or[2]}")
 
+
+def simple_error(inputs : np.array, expected : np.array, w : np.array, activation_function):
+    p, dim = inputs.shape # p puntos en el plano, dim dimensiones
+    o = lambda x: activation_function(np.dot(x, w))
+    val = 0.5 * sum((expected[mu] - o(inputs[mu]))**2 for mu in range(p))
+    return val
+
 def ejercicio_2():
     with open("tp3/data.csv") as f:
         data = list(csv.reader(f)) 
@@ -38,46 +45,34 @@ def ejercicio_2():
     inputs = np.concatenate((np.ones((data.shape[0], 1)), data), axis=1)
     # outputs = data[:, -1]
 
-    def linear_error(inputs : np.array, expected : np.array, w : np.array, activation_function):
-        p, dim = inputs.shape # p puntos en el plano, dim dimensiones
-        o = lambda x: activation_function(np.dot(x, w))
-        val = 0.5 * sum((expected[mu] - o(inputs[mu]))**2 for mu in range(p))
-        return val
     
-    # w = train_perceptron(config, inputs, outputs, lambda x: x, linear_error)
+    # w = train_perceptron(config, inputs, outputs, lambda x: x, simple_error)
     # print(w)  
     beta = config["beta"]
+    
+    print("linear")
     # Linear
-    k_fold_cross_validation(config, inputs, af.id, linear_error, af.one)
+    linear_results = k_fold_cross_validation(config, inputs, af.id, simple_error, af.one)
+    process_k_fold_cross_validation_results(linear_results, af.id, af.id, simple_error)
 
     print("tanh")
     # Non linear - tanh
-    tanh_inputs = np.copy(inputs)
-    max_tanh_expected = max(tanh_inputs[:, -1])
-    min_tanh_expected = min(tanh_inputs[:, -1])
-    tanh_min = -1 
-    tanh_max = 1
-    tanh_inputs[:, -1] = af.normalize(tanh_inputs[:, -1], min_tanh_expected, max_tanh_expected, tanh_min, tanh_max) # Normalization
+    analyze_simple_method(config, np.copy(inputs), lambda x: af.tanh(x, beta), lambda x: af.tanh_derivative(x, beta), -1, 1)
 
-    tanh_activation = lambda x: af.tanh(x, beta)
-    denormalize_tanh = partial(af.denormalize, x_min=min_tanh_expected, x_max=max_tanh_expected, a=tanh_min, b=tanh_max)
-    tanh_results = k_fold_cross_validation(config, tanh_inputs, tanh_activation, linear_error, partial(af.tanh_derivative, beta))
-    process_k_fold_cross_validation_results(tanh_results, tanh_activation, denormalize_tanh, linear_error)
-
-    print("logistic")
     # Non linear - logistic
-    logis_inputs = np.copy(inputs)
-    max_logis_expected = max(logis_inputs[:, -1])
-    min_logis_expected = min(logis_inputs[:, -1])
-    logis_min = 0
-    logis_max = 1
-    logis_inputs[:, -1] = af.normalize(logis_inputs[:, -1], min_logis_expected, max_logis_expected, logis_min, logis_max) # Normalization
-
-    logis_activation = lambda x: af.logistic(x, beta)
-    denormalize_logis = partial(af.denormalize, x_min=min_logis_expected, x_max=max_logis_expected, a=logis_min, b=logis_max)
-    logis_results = k_fold_cross_validation(config, logis_inputs, logis_activation, linear_error, partial(af.logistic_derivative, beta))
-    process_k_fold_cross_validation_results(logis_results, logis_activation, denormalize_logis, linear_error)
+    print("logistic")
+    analyze_simple_method(config, np.copy(inputs), lambda x: af.logistic(x, beta), lambda x: af.logistic_derivative(x, beta), 0, 1)
     
+
+def analyze_simple_method(config : dict, inputs : np.array, activation_function, derivative_function, interval_min, interval_max):
+    min_expected = min(inputs[:, -1])
+    max_expected = max(inputs[:, -1])
+    inputs[:, -1] = af.normalize(inputs[:, -1], min_expected, max_expected, interval_min, interval_max) # Normalization
+
+    denormalize = partial(af.denormalize, x_min=min_expected, x_max=max_expected, a=interval_min, b=interval_max)
+    results = k_fold_cross_validation(config, inputs, activation_function, simple_error, derivative_function)
+    process_k_fold_cross_validation_results(results, activation_function, denormalize, simple_error)
+
 
 def process_k_fold_cross_validation_results(results, activation_function, denormalize_function, error_function):
     errors = []
@@ -85,9 +80,6 @@ def process_k_fold_cross_validation_results(results, activation_function, denorm
         errors.append(error_function(test[:, :-1], denormalize_function(test[:, -1]), w, lambda x: denormalize_function(activation_function(x))))
         print(w)
     print(errors)
-
-
-
 
 
 def k_fold_cross_validation(config, inputs, activation_function, error_function, deriv_activation_function = lambda x: 1):
@@ -99,8 +91,8 @@ def k_fold_cross_validation(config, inputs, activation_function, error_function,
     # test_errors = []
     results = []
     for i in range(k):
-        train = np.concatenate((inputs[:i*fold_size], inputs[(i+1)*fold_size:]), axis=0)
         test = inputs[i*fold_size:(i+1)*fold_size]
+        train = np.concatenate((inputs[:i*fold_size], inputs[(i+1)*fold_size:]), axis=0)
         w = train_perceptron(config, train[:, :-1], train[:, -1], activation_function, error_function, deriv_activation_function)
         results.append((w, test))
         # test_errors.append((w, error_function(test[:, :-1], test[:, -1], w, activation_function)))
@@ -117,7 +109,6 @@ def k_fold_cross_validation(config, inputs, activation_function, error_function,
     # print(err_min)
     # print(f"x3 = {-w_min[2]/w_min[3]}*x2 + {-w_min[1]/w_min[3]}*x1 + {-w_min[0]/w_min[3]}")
     # return sum(errors) / k 
-
 
 
 if __name__ == "__main__":
