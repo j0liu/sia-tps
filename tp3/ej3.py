47a1009c5aaa68c9c1ec79ab3925376562bad129
davidwis <davidwis000@gmail.com>
2024-04-30 19:22:40 -0300

    Continuacion del perceptron multilayer

--- tp3/ej3.py
+++ tp3/ej3.py
@@ -1,25 +1,29 @@
 import numpy as np 
 import activation_functions as af
-from perceptron import train_multilayer_perceptron, forward_propagation
+from perceptron import train_multilayer_perceptron, multi_error, forward_propagation, layer_normalize
 import json
 
 with open("tp3/config.json") as f:
     config = json.load(f)
 
-def multi_error(inputs : np.array, expected : np.array, w : np.array, activation_function):
-    p, dim = inputs.shape # p puntos en el plano, dim dimensiones
-    
-    o = lambda x: activation_function(np.dot(x, w)) + config["bias"]
-    val = 0.5 * sum((expected[mu] - o(inputs[mu]))**2 for mu in range(p))
-    return val
+
+def output(x, layer_sizes, w, activation_function):
+    return forward_propagation(x, layer_sizes, w, activation_function)[-1][1:]
+
 
 def ejercicio_3():
     inputs = np.array([[1, -1, 1], [1, 1, -1], [1, -1, -1], [1, 1, 1]])
     expected_or = np.array([[1], [1], [-1], [-1]])
 
-    data = parse_to_matrices('tp3\TP3-ej3-digitos.txt')
-    w_or = train_multilayer_perceptron(config, inputs, [3,2,1], expected_or, af.id)
+    step_function = lambda x: 1 if x >= 0 else -1
+
+    # data = parse_to_matrices('tp3/TP3-ej3-digitos.txt')
+    layer_sizes = np.array(layer_normalize([2,2,1]))
+    w_or = train_multilayer_perceptron(config, inputs, layer_sizes, expected_or, af.step)
     print(w_or)
+    print("val:", multi_error(np.array([[1, -1, 1]]), np.array([[1]]), layer_sizes, w_or, af.step))
+    for x in inputs:
+        print("x", x, "f(x)=", output(x, layer_sizes, w_or, af.step))
 
 
 def parse_to_matrices(filepath, columns=5, rows=7):
--- tp3/perceptron.py
+++ tp3/perceptron.py
@@ -3,10 +3,21 @@ import sys
 from functools import partial
 from plot import plotxy
 import json
+from utils import pad
+import math
 
 with open("tp3/config.json") as f:
     config = json.load(f)
 
+
+def layer_normalize(layer_sizes : np.array):
+    return list(map(lambda x : x+1, layer_normalize))
+
+def simple_error(inputs : np.array, expected : np.array, w : np.array, activation_function):
+    p, dim = inputs.shape # p puntos en el plano, dim dimensiones
+    o = lambda x: activation_function(np.dot(x, w))
+    val = 0.5 * sum((expected[mu] - o(inputs[mu]))**2 for mu in range(p))
+    return val
 
 
 #puntos en el plano: n = 2
@@ -14,7 +25,7 @@ with open("tp3/config.json") as f:
 #activation_function: theta
 #expected: tzeta
 #se debe tomar epsilon 0 para step
-def train_perceptron(config : dict, inputs : np.array, expected_results : np.array, activation_function, error_function, deriv_activation_function = lambda x: 1):
+def train_perceptron(config : dict, inputs : np.array, expected_results : np.array, activation_function, error_function = simple_error, deriv_activation_function = lambda x: 1):
     p, dim = inputs.shape # p puntos en el plano, dim dimensiones
 
     i = 0
@@ -26,25 +37,29 @@ def train_perceptron(config : dict, inputs : np.array, expected_results : np.arr
         h = np.dot(inputs[mu],w)
         o = activation_function(h)
 
-        delta_w = config['learning_rate'] * (expected_results[mu] - o) * deriv_activation_function(h) * inputs[mu][1:]
+        delta_w = config['learning_rate'] * (expected_results[mu] - o) * deriv_activation_function(h) * inputs[mu]
         w += delta_w
-        # error_o = 0.5 * np.sum((expected_results[mu] - o[mu]) ** 2)
         error = error_function(inputs, expected_results, w, activation_function)
         if error < min_error:
             min_error = error
             w_min = w
-        #plotxy(inputs[:,1],expected_results,w,delta_w)
         i += 1
     return w_min
 
 
-def train_multilayer_perceptron(config : dict, inputs : np.array, layer_sizes : np.array, expected_results : np.array, activation_function, error_function, deriv_activation_function = lambda x: 1):
+def initialize_weights(layer_sizes : np.array, w : np.array, config : dict):
+    for m in range(len(layer_sizes)-1): #sin contar la capa de output
+        for j in range(layer_sizes[m+1]):
+            w[m][j][0] = config['bias']
+            w[m][j][1:layer_sizes[m]] = np.random.rand(layer_sizes[m]-1)
+
+
+def train_multilayer_perceptron(config : dict, inputs : np.array, layer_sizes : np.array, expected_results : np.array, activation_function, deriv_activation_function = lambda x: 1):
     """
     inputs: np.array - matrix of shape p x n, of inputs
     layer_sizes: np.array - array of shape m, with layer sizes
     expected: np.array - array of shape p, of expected outputs
     activation_function: function - R -> R , unless normalized
-    error_function: function - 
     deriv_activation_function: function 
     return: np.array - tensor of shape m x wd x wd
     """
@@ -53,12 +68,12 @@ def train_multilayer_perceptron(config : dict, inputs : np.array, layer_sizes : 
     network_width = max(layer_sizes)
 
     i = 0
-    w = np.zeros((len(layer_sizes)-1, network_width, network_width))# np.concatenate((np.array([config['bias']]), (np.random.rand(dim-1)))) # pesos
+    w = np.zeros((len(layer_sizes)-1, network_width, network_width))
+    initialize_weights(layer_sizes, w, config)
 
     min_error = sys.maxsize
     w_min = None
 
-    level = 0
     while min_error > config['epsilon'] and i < config['limit']:
         mu = np.random.randint(0, p)
         # h = np.dot(inputs[mu],w)
@@ -68,7 +83,7 @@ def train_multilayer_perceptron(config : dict, inputs : np.array, layer_sizes : 
         # delta_w = config['learning_rate'] * (expected_results[mu] - o) * deriv_activation_function(h) * inputs[mu][1:]
         delta_w = backward_propagation(config["learning_rate"], values, layer_sizes, w, expected_results[mu], activation_function, deriv_activation_function)
         w += delta_w
-        error = error_function(inputs, expected_results, w, activation_function)
+        error = multi_error(inputs, expected_results, layer_sizes, w, activation_function)
         if error < min_error:
             min_error = error
             w_min = w
@@ -77,10 +92,11 @@ def train_multilayer_perceptron(config : dict, inputs : np.array, layer_sizes : 
 
 def forward_propagation(x : np.array, layer_sizes : np.array, w : np.array, activation_function):
     network_width = max(layer_sizes)
-    values = np.zeros((len(layer_sizes)+1, network_width))
-    values[0] = np.pad(x, (0, network_width - len(x)), 'constant')
+    values = np.zeros((len(layer_sizes), network_width))
+    values[0] = pad(x, network_width)
+    values[:,0] = 1 #poner 1 a cada valor[m][0] para todo m
     for m in range(1, len(layer_sizes)):
-        for j in range(layer_sizes[m]):
+        for j in range(1,layer_sizes[m]):
             values[m][j] = activation_function(np.dot(values[m-1], w[m-1][j]))
     return values
 
@@ -89,17 +105,37 @@ def backward_propagation(learning_rate : float, values : np.array, layer_sizes :
 
     deltas = np.zeros((len(layer_sizes)-1, network_width))
     delta_ws = np.zeros((len(layer_sizes)-1, network_width, network_width))
-    expected = np.pad(expected, (0, network_width - len(expected)), 'constant')
+    # expected = np.pad(expected, (0, network_width - len(expected)), 'constant')
+
+    # expected = pad(expected, network_width-1)
+    expected_copy = np.zeros(network_width)
+    expected_copy[0] = 1
+    expected_copy[1:] = pad(expected, network_width-1)
 
     #base case
     der_act_h = np.array([deriv_activation_function(np.dot(values[-2], w[-1][i])) for i in range(layer_sizes[-1])]) #deriv_activation_function(h)
-    deltas[-1] = (expected - values[-1]) * der_act_h
-    delta_ws[-1] = learning_rate * deltas[-1] * values[-1]
+    deltas[-1] = (expected_copy - values[-1]) * pad(der_act_h, network_width)
+    delta_ws[-1] = learning_rate * deltas[-1] * values[-2]
 
     for m in range(len(deltas)-2, -1, -1):
         for j in range(layer_sizes[m]):
             h = np.dot(values[m], w[m][j])
+
             deltas[m][j] = (np.dot(deltas[m+1], w[m+1][j])) * deriv_activation_function(h)
+            
             delta_ws[m][j] = learning_rate * deltas[m][j] * values[m]
     
     return delta_ws
+
+
+def multi_error(inputs : np.array, expected_results : np.array, layer_sizes : np.array, w : np.array, activation_function):
+    p, _ = inputs.shape # p puntos en el plano, dim dimensiones
+
+    val = 0
+    for mu in range(p):
+        output = forward_propagation(inputs[mu], layer_sizes, w, activation_function)[-1][1:]
+        for i in range(len(expected_results[mu])):
+            val += 0.5 * (expected_results[mu][i] - output[i])**2
+    # val = 0.5 * sum(sum((expected_results[mu][i] - o(inputs[mu][i]))**2 for i in range(1, len(expected_results[mu]) + 1)) for mu in range(p))
+    # print(val)  
+    return val
